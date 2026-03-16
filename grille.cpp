313fee064688ccf0bb9d51633aa7b701cd2dd4ba
palex44 <alexandre.peytavi@ecoles-epsi.net>
2026-03-16 09:13:05 +0100

    bonus points, tp, cerise

--- grille.cpp
+++ grille.cpp
@@ -8,36 +8,49 @@
 #include <iostream>
 
 Grille::Grille(int t)
-:   tileSize(t),
-    wall(sf::Vector2f((float)t, (float)t)),
-    pellet(t * 0.12f)
+: tileSize(t),
+  wall(sf::Vector2f((float)t, (float)t)),
+  pellet1(t * 0.12f),
+  pellet2(t * 0.24f),
+  pellet3(t * 0.24f),
+  pellet4(t * 0.24f)
 {
     map = { // plateau de jeu
-        "############################",
-        "#............##............#",
-        "#.####.#####.##.#####.####.#",
-        "#.#  #.#   #.##.#   #.#  #.#",
-        "#.####.#####.##.#####.####.#",
-        "#..........................#",
-        "#.####.##.########.##.####.#",
-        "#......##....##....##......#",
-        "######.#####.##.#####.######",
-        "#............##............#",
-        "#.####.#####.##.#####.####.#",
-        "#...##................##...#",
-        "###.##.##.#########.##.##.##",
-        "#......##....##....##......#",
-        "#.##########.##.##########.#",
-        "#..........................#",
-        "############################"
-    };
+    "############################",
+    "#............##............#",
+    "#.####.#####.##.#####.####.#",
+    "#.####.#####.##.#####.####.#",
+    "#..........................#",
+    "#.####.##.########.##.####.#",
+    "#.####.##.########.##.####.#",
+    "#..b...##....##....##...b..#",
+    "######.#####.##.#####.######",
+    " ............##............ ",
+    "#.####.#####....#####.####.#",
+    "#.####.##   #--#   ##.####.#",
+    "#......##   #  #   ##..b...#",
+    "#.######### #  # #########.#",
+    "#.######### #### #########.#",
+    "#...........    ...........#",
+    "#.####.##.########.##.####.#",
+    "#.####.##.########.##.####.#",
+    "#..v....................b..#",
+    "############################",
+}
+;
 
     // créartion du mur
     wall.setFillColor(sf::Color::Blue);
 
-    // création point
-    pellet.setFillColor(sf::Color::White);
-    pellet.setOrigin({pellet.getRadius(), pellet.getRadius()}); // utile pour centrer dans la case les points
+    //création point
+    pellet1.setFillColor(sf::Color::Yellow); // jaune
+    pellet1.setOrigin({pellet1.getRadius(), pellet1.getRadius()}); // centrer le point
+    pellet2.setFillColor(sf::Color::Yellow); // jaune
+    pellet2.setOrigin({pellet2.getRadius(), pellet2.getRadius()}); // centrer le point
+    pellet3.setFillColor(sf::Color(144, 238, 144)); // vert pastelle
+    pellet3.setOrigin({pellet3.getRadius(), pellet3.getRadius()}); // centrer le point
+    pellet4.setFillColor(sf::Color::Red); // rouge
+    pellet4.setOrigin({pellet4.getRadius(), pellet4.getRadius()}); // centrer le point
 }
 
 // fonction qui dessine la grille (PAS de boucle window ici)
@@ -61,21 +74,66 @@ void Grille::draw(sf::RenderWindow& window)
             // si point
             else if (c == '.')
             {
-                pellet.setPosition({pos.x + tileSize/2.f, pos.y + tileSize/2.f}); // centrer le point
-                window.draw(pellet);
+                pellet1.setOrigin({pellet1.getRadius(), pellet1.getRadius()}); // centrer le point
+                pellet1.setPosition({pos.x + tileSize/2.f, pos.y + tileSize/2.f}); // centrer le point
+                window.draw(pellet1);
+            }
+            // si bonus point
+            else if (c == 'b')
+            {
+                pellet2.setOrigin({pellet2.getRadius(), pellet2.getRadius()}); // centrer le point
+                pellet2.setPosition({pos.x + tileSize/2.f, pos.y + tileSize/2.f}); // centrer le point
+                window.draw(pellet2);
+            }
+            // si super bonus vie
+            else if (c == 'v')
+            {
+                pellet3.setOrigin({pellet3.getRadius(), pellet3.getRadius()}); // centrer le point
+                pellet3.setPosition({pos.x + tileSize/2.f, pos.y + tileSize/2.f}); // centrer le point
+                window.draw(pellet3);
+            }
+            // si cerise
+            else if (c == 'c')
+            {
+                pellet4.setOrigin({pellet4.getRadius(), pellet4.getRadius()}); // centrer le point
+                pellet4.setPosition({pos.x + tileSize/2.f, pos.y + tileSize/2.f}); // centrer le point
+                window.draw(pellet4);
             }
         }
     }
 }
 
-void Grille::point(int x, int y)
+
+int Grille::point(int x, int y)
 {
-    int score = 0;
-    if (map[y][x] == '.')
+    if (map[y][x] == '.') 
     {
-        map[y][x] = ' '; // On remplace le point par un espace pour indiquer qu'il a été mangé
-        score++;
+        map[y][x] = ' ';
+        return 100; // point normal
+    }      
+
+    else if (map[y][x] == 'b') 
+    {
+        map[y][x] = ' ';
+        bonuspoint++;
+        // apparition cerise après 2 gros bonus
+        if (bonuspoint == 2) {
+            map[15][14] = 'c'; // position de la cerise
+        }
+        return 500;
     }
+    else if (map[y][x] == 'v') 
+    {
+        map[y][x] = ' ';
+        bonuspoint++;        
+        return 1; // vie bonus
+    }
+    else if (map[y][x] == 'c') 
+    {
+        map[y][x] = ' ';
+        return 1000; // cerise
+    }
+    return 0;
 }
 
 // ligne
@@ -90,12 +148,14 @@ int Grille::cols() const
     return (int)map[0].size();
 }
 
-// foncer dans les murs
+// fonction pour les murs
 bool Grille::isWall(int x, int y) const
 {
-    // sécurité :: si on sort de la grille 
-    if (x < 0 || x >= cols() || y < 0 || y >= rows())
-        return true;
-    // vérifier si la case contient un mur
-    return map [y][x] == '#';
+    if (x < 0 || x >= cols() || y < 0 || y >= rows()){
+        return false; // pas de mur mais hors de la grille --> on peut se tp
+    }
+    if (map[y][x] == '#'){
+        return true; // mur donc collision
+    }
+    return false; // pas de mur
 }
--- grille.hpp
+++ grille.hpp
@@ -6,16 +6,19 @@
 class Grille {
 public: // ce qui va être partagé aux autres codes 
     Grille(int tileSize); // constructeur
-    void draw(sf::RenderWindow& window); // afficher le plateau 
-    void point(int x, int y); // pour manger les points
+    void draw(sf::RenderWindow& window); // afficher le plateau
+    int point(int x, int y); // pour manger les points
     int rows() const; // dimensionner la fenêtre
     int cols() const; // egal
-    bool isWall(int x, int y) const; // ne pas traverser les murs 
-
-private: // ce qui appartient à la grille en interne 
+    bool isWall(int x, int y) const; // pour les collisions avec les murs
+    int bonuspoint = 0; // variable globale pour compter les bonus points
+    
+    private: // ce qui appartient à la grille en interne 
     std::vector<std::string> map; // la carte
-    int tileSize; // taille des tuiles 
-
-    sf::RectangleShape wall; // forme du mur 
-    sf::CircleShape pellet; // forme de la nourriture
+    int tileSize; // taille des tuiles
+    sf::RectangleShape wall; // forme du mur
+    sf::CircleShape pellet1; // forme du point normal
+    sf::CircleShape pellet2; // forme du gros point
+    sf::CircleShape pellet3; // forme bonus vie
+    sf::CircleShape pellet4; // forme bonus cerise
 };
